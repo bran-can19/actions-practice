785b7a14c2dd143128555dc34c899c4955459293
bran-can19 <Candanedabrandon@gmail.com>
2026-06-17 15:41:55 -0600

    update actions for python2.0

--- .github/app.py
+++ .github/app.py
@@ -1,13 +1,15 @@
 import os
 
-print("Hello, World!")
-
-for i in (1, 2, 3, 4, 5):
-    print("Xd")
-
 def main():
-    username = os.getenv("USER", "Invitado")
-    print(f"Hello, {username}!")
+    print ("Hello World runs on Action!")
+
+    for i in [1, 2, 3, 4, 5]:
+        print ("XD " * i)
+
+    
+    name = os.getenv("USERNAME")
+    print (f"Hello { name }")
+
 
 if __name__ == "__main__":
     main()
